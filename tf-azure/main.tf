7aafcb64f433f174c3d699c23a3507eb6c7f94de
Adrian Morgenegg <adrian@software.catering>
2025-05-23 15:09:58 +0200

    clean up

--- tf-azure/main.tf
+++ tf-azure/main.tf
@@ -95,6 +95,7 @@ resource "azurerm_linux_web_app" "webapp" {
   }
 }
 
+## Is not working. Use git hub actions instead. Or with Zip upload, check on other repo https://github.com/AdrianM/todo-app-web.git.
 resource "azurerm_app_service_source_control" "sourcecontrol" {
   app_id             = azurerm_linux_web_app.webapp.id
   repo_url           = "https://github.com/AdrianM/todo-app-web.git"  # You'll need to update this
